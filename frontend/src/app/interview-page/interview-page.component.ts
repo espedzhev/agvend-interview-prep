04e88cb2bf88aa6e4c8c22a4d32e005e568f03bb
Evald <evald.spedzhev@gmail.com>
2025-05-14 22:51:20 +0100

    added bootstraps

--- frontend/src/app/interview-page/interview-page.component.ts
+++ frontend/src/app/interview-page/interview-page.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from "@angular/common"
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-interview-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './interview-page.component.html',
   styles: ``
 })
 export class InterviewPageComponent {
+   private readonly http = inject(HttpClient)
 
 }
